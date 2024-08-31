66e3e67d2c0c000cbd46a1f9cd14928b02ab0f2d
Santiago Garbini <sgarbini@uade.edu.ar>
2024-08-31 10:01:59 -0300

    constantes
    Cambio de variables por constantes

--- TPO11.py
+++ TPO11.py
@@ -14,19 +14,20 @@ def verify_guess(word):
 
 #PROGRAMA PRINCIPAL
 
-lista_palabras_posibles = ["arbol","amigo","cajon","clase","boton"]
+LISTA_PALABRAS_POSIBLES = ["arbol","amigo","cajon","clase","boton"]
 
-palabra_secreta = lista_palabras_posibles[random.randint(0,len(lista_palabras_posibles))]
-intentos_maximos = 6
+palabra_secreta = LISTA_PALABRAS_POSIBLES[random.randint(0,len(LISTA_PALABRAS_POSIBLES))]
+INTENTOS_MAXIMO = 6
 
 
 
 
 print("BIENVENIDO A STRINGLE\nEl juego consiste en adivinar una palabra de 5 letras, para ello contas con 6 intentos, despues de cada intento se te informara que letras estan en la palabra y se encuentran en la posicion correcta y cuales forman parte de la palabra, pero estan en la posicion incorrecta")
 
-for intentos in range (1, intentos_maximos+1):
+for intentos in range (1, INTENTOS_MAXIMO+1):
     palabra = input(f"INTENTO N° {intentos}\nIngrese una palabra de 5 letras:  ")
     palabra.lower
     palabra = verify_guess(palabra)
+    
 
 print(palabra)
