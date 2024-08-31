a61ba454defc103bd7795588468251b303fde5e4
Santiago Garbini <sgarbini@uade.edu.ar>
2024-08-31 09:23:57 -0300

    update consigna del juego
    Explicación de como funciona el juego

--- TPO11.py
+++ TPO11.py
@@ -15,7 +15,7 @@ def verify_guess(word):
 
 lista = ["arbol","amigo","cajon","clase","boton"]
 
-print("BIENVENIDO A STRINGLE\nEl juego consiste en...")
+print("BIENVENIDO A STRINGLE\nEl juego consiste en adivinar una palabra de 5 letras, para ello contas con 6 intentos, despues de cada intento se te informara que palabras se encuentran en la posicion correcta y cuales forman parte de la palabra, pero estan en la posicion incorrecta")
 palabra = input(" ")
 
 hola = verify_guess(palabra)
