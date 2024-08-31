dd518ce373f8c0713758280b0c1707d7b57b3516
Santiago Garbini <sgarbini@uade.edu.ar>
2024-08-31 09:57:49 -0300

    validación e ingreso
    Corrección de la validación de la palabra ingresada
    Ingreso por intentos

--- TPO11.py
+++ TPO11.py
@@ -4,18 +4,29 @@ import re
 #FUNCIONES
 
 def verify_guess(word):
-    if word == "":
-        word = input("Ingrese una palabra. ")
-    elif len(word) != 5:
-        word = input("Ingrese una palabra de 5 letras. ") 
-    elif word.isdigit() == True:
-        word = input("Ingrese una palabra válida. ")
+    while word.isalpha() == False:
+            word = input("La palabra no puede contener numeros. Ingrese una palabra válida. ")
+    while word == "":
+        word = input("No se ingreso ninguna palabra. Ingrese una palabra. ")
+    while len(word) != 5:
+        word = input("Longitud incorrecta. Ingrese una palabra de 5 letras. ") 
+    
 
 #PROGRAMA PRINCIPAL
 
-lista = ["arbol","amigo","cajon","clase","boton"]
+lista_palabras_posibles = ["arbol","amigo","cajon","clase","boton"]
 
-print("BIENVENIDO A STRINGLE\nEl juego consiste en adivinar una palabra de 5 letras, para ello contas con 6 intentos, despues de cada intento se te informara que palabras se encuentran en la posicion correcta y cuales forman parte de la palabra, pero estan en la posicion incorrecta")
-palabra = input(" ")
+palabra_secreta = lista_palabras_posibles[random.randint(0,len(lista_palabras_posibles))]
+intentos_maximos = 6
 
-hola = verify_guess(palabra)
+
+
+
+print("BIENVENIDO A STRINGLE\nEl juego consiste en adivinar una palabra de 5 letras, para ello contas con 6 intentos, despues de cada intento se te informara que letras estan en la palabra y se encuentran en la posicion correcta y cuales forman parte de la palabra, pero estan en la posicion incorrecta")
+
+for intentos in range (1, intentos_maximos+1):
+    palabra = input(f"INTENTO N° {intentos}\nIngrese una palabra de 5 letras:  ")
+    palabra.lower
+    palabra = verify_guess(palabra)
+
+print(palabra)
