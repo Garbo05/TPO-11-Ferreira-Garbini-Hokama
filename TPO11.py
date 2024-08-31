756314f92e268854bed9c175ce69ea5c853884f1
Santiago Garbini <sgarbini@uade.edu.ar>
2024-08-31 13:26:16 -0300

    logica del juego

--- TPO11.py
+++ TPO11.py
@@ -1,7 +1,11 @@
 import random
 import re
+from colorama import Fore, init
 
 #FUNCIONES
+
+def print_variables(a):
+    print(a)
 
 def verify_guess(word):
     while word.isalpha() == False:
@@ -10,14 +14,17 @@ def verify_guess(word):
         word = input("No se ingreso ninguna palabra. Ingrese una palabra. ")
     while len(word) != 5:
         word = input("Longitud incorrecta. Ingrese una palabra de 5 letras. ") 
+    return word
     
 
 #PROGRAMA PRINCIPAL
 
-LISTA_PALABRAS_POSIBLES = ["arbol","amigo","cajon","clase","boton"]
-
-palabra_secreta = LISTA_PALABRAS_POSIBLES[random.randint(0,len(LISTA_PALABRAS_POSIBLES))]
+LISTA_PALABRAS_POSIBLES = ["ARBOL","AMIGO","CAJON","CLASE","BOTON"]
+print_variables(LISTA_PALABRAS_POSIBLES)
+palabra_secreta = LISTA_PALABRAS_POSIBLES[random.randint(0,len(LISTA_PALABRAS_POSIBLES)-1)]
+print_variables(palabra_secreta)
 INTENTOS_MAXIMO = 6
+#Inicializacion de la ista en la que se van a agregar las letras que se encuenten en la palabra  
 
 
 
@@ -26,8 +33,24 @@ print("BIENVENIDO A STRINGLE\nEl juego consiste en adivinar una palabra de 5 let
 
 for intentos in range (1, INTENTOS_MAXIMO+1):
     palabra = input(f"INTENTO N° {intentos}\nIngrese una palabra de 5 letras:  ")
-    palabra.lower
     palabra = verify_guess(palabra)
-    
+    palabra.upper()
+    print_variables(palabra)
+    resultado=[]
+    for i in range (len(palabra)):
+        if palabra[i]==palabra_secreta[i]:
+            #letra_verde=Fore.GREEN+palabra[i]
+            resultado.append(Fore.GREEN+palabra[i])
+        elif palabra[i] in palabra_secreta:
+            #letra_amarilla=Fore.YELLOW+palabra[i]
+            resultado.append(Fore.YELLOW+palabra[i])
+        else:
+            # letra_roja=Fore.RED+palabra[i]
+            resultado.append(letra_roja=Fore.RED+palabra[i])
+    print(resultado)
+if palabra==palabra_secreta:
+    print(f"La palabra secreta era {palabra}. Felicidades")
+else:
+    print(f"Mala suerte, se agotaron tus {INTENTOS_MAXIMO} intentos, la palabra secreta era {palabra_secreta}.")                           
 
-print(palabra)
+#print(Fore.GREEN + palabra)
