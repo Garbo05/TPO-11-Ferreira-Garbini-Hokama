190988e91e8c552fab1199f1aa72ac57f4db8bf2
tomyhok <thokama@uade.edu.ar>
2024-09-02 00:45:06 -0300

    Update TPO11.py

--- TPO11.py
+++ TPO11.py
@@ -9,9 +9,9 @@ def print_variables(a):
 
 def verify_guess(word):
     while word.isalpha() == False:
-            word = input("La palabra no puede contener numeros. Ingrese una palabra válida. ")
+            word = input("La palabra no puede contener números. Ingrese una palabra válida. ")
     while word == "":
-        word = input("No se ingreso ninguna palabra. Ingrese una palabra. ")
+        word = input("No se ingresó ninguna palabra. Ingrese una palabra. ")
     while len(word) != 5:
         word = input("Longitud incorrecta. Ingrese una palabra de 5 letras. ") 
     return word
@@ -20,30 +20,30 @@ def verify_guess(word):
 #PROGRAMA PRINCIPAL
 
 LISTA_PALABRAS_POSIBLES = ["ARBOL","AMIGO","CAJON","CLASE","BOTON"]
-print_variables(LISTA_PALABRAS_POSIBLES)
-palabra_secreta = LISTA_PALABRAS_POSIBLES[random.randint(0,len(LISTA_PALABRAS_POSIBLES)-1)]
-print_variables(palabra_secreta)
+#print_variables(LISTA_PALABRAS_POSIBLES)
+palabra_secreta = random.choice(LISTA_PALABRAS_POSIBLES)
+#print_variables(palabra_secreta)
 INTENTOS_MAXIMO = 6
-#Inicializacion de la ista en la que se van a agregar las letras que se encuenten en la palabra  
+#Inicializacion de la lista en la que se van a agregar las letras que se encuenten en la palabra  
 
 
 
 
-print("BIENVENIDO A STRINGLE\nEl juego consiste en adivinar una palabra de 5 letras, para ello contas con 6 intentos, despues de cada intento se te informara que letras estan en la palabra y se encuentran en la posicion correcta y cuales forman parte de la palabra, pero estan en la posicion incorrecta")
+print("BIENVENIDO A STRINGLE!\nEl juego consiste en adivinar una palabra de 5 letras, para ello cuentas con 6 intentos, despues de cada intento se te informará qué letras se encuentran en la palabra y si se encuentran en la posicion correcta, y cuáles forman parte de la palabra, pero estan en la posicion incorrecta")
 
 for intentos in range (1, INTENTOS_MAXIMO+1):
-    palabra = input(f"\nINTENTO N° {intentos}\nIngrese una palabra de 5 letras: ")
+    palabra = input(f"\nINTENTO N°{intentos}: ")
     palabra = verify_guess(palabra)
-    palabra=palabra.upper()
-    print_variables(palabra)
+    palabra = palabra.upper()
+    #print_variables(palabra)
 
-    resultado=[]
+    resultado = []
 
-    for i in range (len(palabra)):
+    for i in range(len(palabra)):
         if palabra[i]==palabra_secreta[i]:
             letra_verde=Fore.GREEN+palabra[i]
             resultado.append(letra_verde)
-        elif palabra[i] in palabra_secreta:
+        elif (palabra[i] in palabra_secreta) and palabra[i] != palabra_secreta[i]:
             letra_amarilla=Fore.YELLOW+palabra[i]
             resultado.append(letra_amarilla)
         else:
@@ -53,6 +53,8 @@ for intentos in range (1, INTENTOS_MAXIMO+1):
         print(f"{resultado[i]}",end=" ")
 
     if palabra==palabra_secreta:
-        print(f"La palabra secreta era {palabra}. Felicidades")
-else:
-    print(f"Mala suerte, se agotaron tus {INTENTOS_MAXIMO} intentos, la palabra secreta era {palabra_secreta}.")
+        print(f"\nLa palabra secreta era {palabra}. Felicitaciones!")
+        exit()
+
+if palabra != palabra_secreta:
+    print(f"\nMala suerte, se agotaron tus {INTENTOS_MAXIMO} intentos, la palabra secreta era {palabra_secreta}.")
