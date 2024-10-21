7991d63ab02e743c9e511d441687a30eb93c77e3
Santiago Garbini <sgarbini@uade.edu.ar>
2024-10-21 20:18:19 -0300

    Separacion de Pruebas Unitarias

--- Test_Validaciones.py
+++ Test_Validaciones.py
@@ -1,4 +1,5 @@
 """  
+modo oscuro y claro dependiedo de la hora
 Solo letras
 Longitud de 5 caracteres
 longitud menor a 5
@@ -7,6 +8,12 @@ no numeros
 no caracteres especiales
 palabras en la lista
 palabras fuera de la lista
+comprobar coincidencia de 1 letra
+                          2 letras
+                          3 letras
+                          4 letras
+                          5 letras
+
 """
 import unittest
 from Validaciones import cargar_palabras, validate_guess, validate_palabra, remove_accents
@@ -21,15 +28,35 @@ class TestStringle(unittest.TestCase):
         self.assertEqual(remove_accents('ÉXITO'), 'EXITO')
         self.assertEqual(remove_accents('GATOS'), 'GATOS')  
 
-    def test_validate_guess(self):
+    def test_validate_length_guess_equals_five(self):
         self.assertTrue(validate_guess('CASAS'))
-        self.assertFalse(validate_guess('CASA')) 
+
+    def test_validate_length_guess_lest_than_five(self): 
+        self.assertFalse(validate_guess('CASA'))
+
+    def  test_validate_length_guess_more_than_five(self):
         self.assertFalse(validate_guess('CASASS'))
-        self.assertFalse(validate_guess('CASA1')) 
+
+    def test_validate_guess_is_all_letters(self):
+        self.assertTrue(validate_guess('CASAS')) 
+    
+    def test_validate_guess_have_numbers(self):
+        self.assertFalse(validate_guess('CASA0'))
+
+    def test_validate_guess_have_special_characters(self):  
         self.assertFalse(validate_guess('CA$AS')) 
 
-    def test_validate_palabra(self):
+    def test_validate_guess_match_one_letter(self):
+        for i in 'CASAS':
+            if i == 'C':
+                first_letter = 'C'
+        self.assertEqual(first_letter, 'C')
+
+    def test_validate_palabra_in_text_file(self):
         self.assertTrue(validate_palabra(self.palabras, 'CASAS'))
+    
+    def test_validate_word_not_in_text_file(self):
+        self.assertFalse(validate_palabra(self.palabras,'AAAAA'))
 
 if __name__ == '__main__':
     unittest.main()
